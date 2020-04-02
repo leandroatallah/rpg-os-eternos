3e645f5d54e0aa2099e8cde79a5977bc7b2fd1da
leandro atallah <leandroatallah@gmail.com>
2020-04-02 20:06:25 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {createGlobalStyle} from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    border: 0;
+    list-style:none;
+
+  }
+  html, body {
+    height: 100vh;
+  }
+  
+
+  header {
+    height: 40px;
+    background: #f00;
+  }
+`
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header></header>
+
+      <GlobalStyle />
     </div>
   );
 }
 
-export default App;
+export default App
